e21d0d331bf305d7c284682ecb359cf0b3efb3b1
Philip Jasionowski <philip.jasionowski@gmail.com>
2024-04-21 16:12:59 -0400

    Added fredresponse function

--- fredagainbot.py
+++ fredagainbot.py
@@ -4,12 +4,22 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
+token = os.getenv('FRED_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+
+fredResponses = ('can you not ping me im djing', 'look im busy right now can we talk later', 'check this video out: https://www.youtube.com/watch?v=6b7C79Vxk6Q',
+ 'mfmmmfmm')
+
+
+def fredOutput():
+    if random.randint(0,len(fredResponses))==0:
+        return f'check out this cool number: {random.randint(1,100)}'
+    return random.choice(fredResponses) 
+    
 
 @client.event
 async def on_ready():
@@ -24,9 +34,14 @@ async def on_message(message):
         await message.channel.send('hey its me fred again')
         return
     
+    if client.application_id in map(lambda x: x.id, message.mentions):
+        await message.chanel.send('what do you want')
+
     if random.random() < 0.02:
         embed = discord.Embed()
         embed.set_image(url='https://i.postimg.cc/h4ZvQmzJ/Fred-Again.jpg')
         await message.channel.send(embed=embed)
+
+    
 
 client.run(token)
